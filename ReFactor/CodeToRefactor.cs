c14f0287e6db3970d5df8c3b079804dfdd1c8d8d
Don <don@delegia.se>
2023-10-25 12:38:47 +0800

    separated People object

--- ReFactor/CodeToRefactor.cs
+++ ReFactor/CodeToRefactor.cs
@@ -1,26 +1,10 @@
-﻿using System;
+﻿using ReFactor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CodingAssessment.Refactor
 {
-    public class People
-    {
-        private static readonly DateTimeOffset Under16 = DateTimeOffset.UtcNow.AddYears(-15);
-        public string Name { get; private set; }
-        public DateTimeOffset DOB { get; private set; }
-
-        public People(string name) : this(name, Under16.Date)
-        {
-        }
-
-        public People(string name, DateTime dob)
-        {
-            Name = name;
-            DOB = dob;
-        }
-    }
-
     public class BirthingUnit
     {
         /// <summary>
